72f0579fd09b58e2afcc29018299fa9c56dfd817
innovator256 <BM-2cV87rXv7cVFLWZGnSJN14BWHHrG9Aj7Zr@bitmessage.ch>
2015-02-13 06:56:49 -0500

    info on routes

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,44 @@
  * Serve JSON to our AngularJS client
  */
 
-exports.name = function (req, res) {
-  res.json({
-    name: 'Bob'
+// let use a dummny dtatbase for now
+
+var data = {
+  "posts": [
+    {
+      "title": "Lorem ipsum",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      "title": "Sed egestas",
+      "text": "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus."
+    }
+  ]
+};
+
+// GET
+
+exports.posts = function (req, res) {
+  var posts = [];
+  data.posts.forEach(function (post, i) {
+    posts.push({
+      id: i,
+      title: post.title,
+      text: post.text.substr(0, 50) + '...'
+    });
   });
+  res.json({
+    posts: posts
+  });
+};
+
+exports.post = function (req, res) {
+  var id = req.params.id;
+  if (id >= 0 && id < data.posts.length) {
+    res.json({
+      post: data.posts[id]
+    });
+  } else {
+    res.json(false);
+  }
 };
