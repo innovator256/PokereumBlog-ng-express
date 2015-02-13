c83da9a715399157eb79f85043bef0d43ca10d95
innovator256 <BM-2cV87rXv7cVFLWZGnSJN14BWHHrG9Aj7Zr@bitmessage.ch>
2015-02-13 06:49:54 -0500

    lovely getter controllers

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,8 +2,9 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, $http) {
+angular.module('myApp.controllers', [])
+
+.controller('AppCtrl', function ($scope, $http) {
 
     $http({
       method: 'GET',
@@ -16,12 +17,63 @@ angular.module('myApp.controllers', []).
       $scope.name = 'Error!';
     });
 
-  }).
-  controller('MyCtrl1', function ($scope) {
-    // write Ctrl here
+  })
 
-  }).
-  controller('MyCtrl2', function ($scope) {
-    // write Ctrl here
+.controller('IndexCtrl', ['$scope','$http', function ($scope, $http) {
+  $http.get('/api/posts').
+    success(function(data, status, headers, config) {
+      $scope.posts = data.posts;
+    });
+  }])
 
-  });
+.controller('AddPostCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+  $scope.form = {};
+  $scope.submitPost = function () {
+    $http.post('/api/post', $scope.form).
+      success(function(data) {
+        $location.path('/');
+      });
+    };
+  }])
+
+.controller('ReadPostCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+ $http.get('/api/post/' + $routeParams.id).
+    success(function(data) {
+      $scope.post = data.post;
+    });
+  }])
+
+.controller('EditPostCtrl', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
+ $scope.form = {};
+  $http.get('/api/post/' + $routeParams.id).
+    success(function(data) {
+      $scope.form = data.post;
+    });
+
+  $scope.editPost = function () {
+    $http.put('/api/post/' + $routeParams.id, $scope.form).
+      success(function(data) {
+        $location.url('/readPost/' + $routeParams.id);
+      });
+  };
+  }])
+
+.controller('DeletePostCtrl', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
+ $http.get('/api/post/' + $routeParams.id).
+    success(function(data) {
+      $scope.post = data.post;
+    });
+
+  $scope.deletePost = function () {
+    $http.delete('/api/post/' + $routeParams.id).
+      success(function(data) {
+        $location.url('/');
+      });
+  };
+
+  $scope.home = function () {
+    $location.url('/');
+  };
+  }]);
+
+
